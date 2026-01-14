9b342c6ddab032d7e01d1256bf2461cff0deefdf
Douglas Figueiró <douglas.figueiro@ecomp.ufsm.br>
2026-01-14 20:10:44 -0300

    Improve result page UX: instant scroll, optimized transitions and visual adjustments

--- imagemproc/forms.py
+++ imagemproc/forms.py
@@ -2,24 +2,34 @@ from django import forms
 from django.core.exceptions import ValidationError
 import os
 
-class ImageUploadForm(forms.Form):
-    image = forms.ImageField()
 
-    def clean_image(self):
-        image = self.cleaned_data.get('image')
-        
-        if image:
+class ImageUploadForm(forms.Form):
+    # O campo não pode ter o widget multiple, vamos tratar múltiplos no template
+    pass
+
+    def clean_images(self):
+        images = self.files.getlist('images')
+
+        if not images:
+            raise ValidationError('Nenhuma imagem foi enviada')
+
+        if len(images) > 50:
+            raise ValidationError('Máximo de 50 imagens por vez')
+
+        valid_extensions = ['.jpg', '.jpeg', '.png', '.bmp', '.tiff', '.tif']
+
+        for image in images:
             # Verificar extensão do arquivo
             ext = os.path.splitext(image.name)[1].lower()
-            valid_extensions = ['.jpg', '.jpeg', '.png', '.bmp', '.tiff', '.tif']
-            
+
             if ext not in valid_extensions:
                 raise ValidationError(
-                    f"Formato de arquivo inválido. Formatos aceitos: {', '.join(valid_extensions)}"
+                    f"Formato inválido em '{image.name}'. Formatos aceitos: {', '.join(valid_extensions)}"
                 )
-            
+
             # Verificar tamanho do arquivo (máximo 10MB)
             if image.size > 10 * 1024 * 1024:
-                raise ValidationError('O arquivo é muito grande. Tamanho máximo: 10MB')
-        
-        return image
+                raise ValidationError(
+                    f"'{image.name}' é muito grande. Tamanho máximo: 10MB")
+
+        return images
--- imagemproc/urls.py
+++ imagemproc/urls.py
@@ -1,9 +1,9 @@
-#from django.urls import path
-#from . import views
+# from django.urls import path
+# from . import views
 #
-#urlpatterns = [
+# urlpatterns = [
 #    path('', views.upload_and_process_save, name='upload_save'),
-#]
+# ]
 
 from django.urls import path
 from . import views
@@ -11,5 +11,10 @@ from . import views
 app_name = 'imagemproc'
 
 urlpatterns = [
-    path('', views.upload_and_process_save, name='upload_save'),
+    path('', views.home, name='home'),
+    path('upload/', views.upload_and_process_save, name='upload_save'),
+    path('process/<uuid:batch_id>/', views.process_batch, name='process_batch'),
+    path('result/<uuid:batch_id>/', views.view_results, name='view_results'),
+    path('download/<uuid:batch_id>/',
+         views.download_batch_zip, name='download_batch'),
 ]
